38cad6e482d52b2fd2945f4adcdb3e1b584db610
renliangbo <renna0508@126.com>
2018-01-04 22:44:56 +0800

    git init

---
+++ src/components/search_bar.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+class SearchBar extends Component {
+		constructor(props) {
+				super(props);
+				this.state = {
+						term: ''
+				};
+		}
+		render() {
+				return (
+						<div>
+								<input
+										value={this.state.term}
+										onChange={(event) => {
+										this.setState({term: event.target.value});
+										console.log(this.state)
+								}}
+										type="text"/>
+								<div>
+										{this.state.term}
+								</div>
+
+						</div>
+				)
+
+		}
+
+		onInputChange(event) {}
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import
+'./index.css';
+import App from './App';
+import registerServiceWorker from
+'./registerServiceWorker';
+const API_KEY = 'AIzaSyAz9ytj8L0G0vQJOC2dms_mPx2HVVdNpDk';
+
+ReactDOM.render(< App / >, document.getElementById('root'));
+
+registerServiceWorker();
