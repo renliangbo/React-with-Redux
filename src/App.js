76c66c39a51fce41c8716d4a166300fe70f00cb2
renliangbo <renna0508@126.com>
2018-01-05 15:45:28 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+
 import './App.css';
+
+const API_KEY = 'AIzaSyAz9ytj8L0G0vQJOC2dms_mPx2HVVdNpDk';
 class App extends Component {
+		constructor(props){
+			super(props);
+			this.state={ 
+				videos:[],
+				selectedVideo:null
+			};
+			YTSearch({key:API_KEY,term:'surfboards'},(videos)=>{
+				this.setState({
+					videos,
+					selectedVideo:videos[0]
+				});
+			})
+			super(props)
+		}
 		render() {
 				return (
 						<div>
+								<VideoDetail video={this.state.selectedVideo}/>
 								<SearchBar/>
+								<VideoList 
+									videos={this.state.videos}
+									onVideoSelect={
+										selectedVideo=>this.setState({selectedVideo})
+									}
+								/>
 						</div>
 				)
 		}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import
-'./index.css';
+import './index.css';
 import App from './App';
-import registerServiceWorker from
-'./registerServiceWorker';
-const API_KEY = 'AIzaSyAz9ytj8L0G0vQJOC2dms_mPx2HVVdNpDk';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(< App / >, document.getElementById('root'));
 
